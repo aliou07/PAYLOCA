c77e9f5e1bb44ce5b96829feb86cad19ccd462ef
aliou07 <kassouwat@gmail.com>
2026-08-31 18:51:37 +0100

    Create App component with initial layout

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{padding: '50px', textAlign: 'center'}}>
+      <h1>PAYLOCA ✅</h1>
+      <p>Le site marche enfin !</p>
+    </div>
+  )
+}
+
+export default App
