b3e0a41ef14b3b469f3c87100b3efa08a3dea279
aliou07 <kassouwat@gmail.com>
2026-09-01 13:29:05 +0100

    Add esbuild config with tsconfig specification
    Added esbuild configuration to specify tsconfig file.

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    tsconfig: 'tsconfig.json' // <- ON FORCE VITE À IGNORER lib/db
+  }
 })
