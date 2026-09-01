0ffede189816367257fadab3e12ebfdd8b34e31d
aliou07 <kassouwat@gmail.com>
2026-09-01 21:07:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    tsconfig: 'tsconfig.json' // <- ON FORCE VITE À IGNORER lib/db
   }
 })
