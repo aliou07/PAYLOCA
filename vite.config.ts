e3811d8e1322468efe1a152f790e9a1ad1900d65
aliou07 <kassouwat@gmail.com>
2026-09-01 23:40:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    // On ignore les erreurs de tsconfig manquant
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
