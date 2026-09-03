704f5bdb5b32e0568082d80cb008c97ca525938c
aliou07 <kassouwat@gmail.com>
2026-09-03 03:09:44 +0100

    Create ScopedLocalData.ts

---
+++ artifacts/niger-habitat/src/lib/ScopedLocalData.ts
@@ -0,0 +1,16 @@
+export function commitAccountScopedResult<T>(
+  ownerId: string,
+  currentUserId: () => string | null,
+  value: T,
+  commit: (value: T) => void,
+): boolean {
+  if (
+    currentUserId()
+      !== ownerId
+  ) {
+    return false;
+  }
+
+  commit(value);
+  return true;
+}
