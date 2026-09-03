4aed189ce3e2a75116e6733d04eac52cdc4aec65
aliou07 <kassouwat@gmail.com>
2026-09-03 01:15:50 +0100

    Add annonces route re-exporting from listings
    Create annonces.ts to re-export from listings.ts.

---
+++ artifacts/api-server/src/routes/annonces.ts
@@ -0,0 +1,9 @@
+/**
+ * Entrée française pour les annonces.
+ *
+ * L’implémentation complète reste dans listings.ts afin de conserver tous les
+ * endpoints et leurs contrats API existants sans duplication de code.
+ */
+
+export { default } from "./listings";
+export * from "./listings";
