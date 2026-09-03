f556e577c2f1038985df62d9f98059a735e145e0
aliou07 <kassouwat@gmail.com>
2026-09-03 20:18:21 +0100

    Update utils.ts

--- artifacts/niger-habitat/src/lib/utils.ts
+++ artifacts/niger-habitat/src/lib/utils.ts
@@ -1,13 +1,7 @@
-import { twMerge } from "tailwind-merge";
-import {
-  clsx,
-  type ClassValue,
-} from "clsx";
+import { twMerge } from 'tailwind-merge';
 
-export function cn(
-  ...inputs: ClassValue[]
-) {
-  return twMerge(
-    clsx(inputs),
-  );
+import { clsx, type ClassValue } from 'clsx';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
