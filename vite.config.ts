380322372154d97e23faf5993e9ef78ca4ef025b
aliou07 <kassouwat@gmail.com>
2026-09-06 20:45:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
+    // Empêche vite d'aller chercher les fichiers de lib/db
     rollupOptions: {
-      external: [] // on laisse vide
+      external: []
     }
   },
   optimizeDeps: {
-    exclude: ['@payloca/db'] // si tu as un workspace
+    exclude: ['@payloca/db'] // si tu utilises le workspace
   }
 })
