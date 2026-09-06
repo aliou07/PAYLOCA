38d28a8152fe292faf177eebe604231f45ca132b
aliou07 <kassouwat@gmail.com>
2026-09-06 19:25:26 +0100

    Update feed.ts

--- lib/db/src/schema/feed.ts
+++ lib/db/src/schema/feed.ts
@@ -1,4 +1,4 @@
-import { index, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { index, pgTable, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const feedTable = pgTable("payloca_feed", {
   id: text("id").primaryKey(),
